09b60850edb32753da2a4ab22b100b7ca75a49af
anna <anna@ptf-lab.com>
2024-12-24 19:43:34 +0400

    Updated setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     description='Soccer Field Segmentation and Camera Calibration',
     long_description='Soccer pitch segmentation and camera calibration in two steps. Step 1, pixelwise segmentation of an broacast image of a soccer game into six different clases defined by the line markings. Step 2, a differential-rendering optimizer that tries to estimate camera parameters from such segementations.Trained on SoccerNet.',
     version='1.0',
-    packages=['soccersegcal', 'sncalib'],
+    packages=['soccersegcal', 'soccernet-calibration'],
     zip_safe=False,
     url='https://github.com/Spiideo/soccersegcal/',
     author='Hakan Ardo',
